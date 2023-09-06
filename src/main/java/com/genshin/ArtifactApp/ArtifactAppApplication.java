8da8676fe1848eb1963d4a765946a5bc973b8ba8
Szymon Szczench <83363990+Mevist@users.noreply.github.com>
2023-09-06 13:18:09 +0200

    Init local commit

---
+++ src/main/java/com/genshin/ArtifactApp/ArtifactAppApplication.java
@@ -0,0 +1,13 @@
+package com.genshin.ArtifactApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genshin/ArtifactApp/ArtifactAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genshin.ArtifactApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
